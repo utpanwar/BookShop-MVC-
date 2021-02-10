e56fec7ffc7679d197fde52af03a901a17422e53
UTKARSH PANWAR <utpanwar@outlook.com>
2021-02-10 22:26:36 +0530

    second commit

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BookShop.Data
+namespace BookShop.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- BulkyBook.Models/ViewModels/ErrorViewModel.cs
+++ BulkyBook.Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace BookShop.Models
+namespace BookShop.Models.ViewModels
 {
     public class ErrorViewModel
     {
