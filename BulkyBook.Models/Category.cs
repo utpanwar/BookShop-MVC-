94b7aa2c2be08047bfcd58f398ae23596287818c
UTKARSH PANWAR <utpanwar@outlook.com>
2021-02-11 20:14:57 +0530

    section 5 completed

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        //id is the primary key
+        [Display(Name="Category name")]
+        [Required]
+        [MaxLength(50)]
+        public int Name { get; set; }
+
+    }
+}
